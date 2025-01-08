1c25867e7e36a075532407e43b98718469f8e3f6
flayamelark <sr.flavioamelark@gmail.com>
2025-01-08 17:00:27 -0300

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,6 @@
+print("Ola Flavio")
+
+ano = float(input("Diga o ano que estamos:"))
+nascimento = float(input("Diga seu ano de nascimento"))
+idade = ano-nascimento
+print(f"Você tem {idade} anos ")
